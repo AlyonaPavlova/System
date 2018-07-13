a9108180b10144e41727510b13d45b0c914a1580
Unknown <alyonabill3@mail.ru>
2018-07-13 15:08:03 +0300

    main was changed

--- main.js
+++ main.js
@@ -1,24 +1,15 @@
 "use strict";
 
-const { Company } = require("./classes");
-const { Director } = require("./classes");
-const { Department } = require("./classes");
-const { WebDepartment } = require("./classes");
-const { MobDepartment } = require("./classes");
-const { QADepartment } = require("./classes");
-const { Project } = require("./classes");
-const { WebProject } = require("./classes");
-const { MobProject } = require("./classes");
-const { Developer } = require("./classes");
-const { WebDeveloper } = require("./classes");
-const { MobDeveloper } = require("./classes");
-const { QASpecialist } = require("./classes");
+const { Company, Director, WebDepartment, MobDepartment, QADepartment } = require("./classes");
 
 function main(n) {
     let myCompany = new Company({"WebDept": new WebDepartment(), "MobDept": new MobDepartment(), "QADept": new QADepartment()}, new Director());
-
+    let dayCounter = 1;
+    
     while (n) {
-        console.log("***start Day ***");
+        console.log("***start Day " + dayCounter + " ***");
+        dayCounter++;
+        console.log("\n");
 
         // Генерация проектов на каждый день. Заполнение массивов в объектах отделов
 
@@ -30,7 +21,7 @@ function main(n) {
 
         // Обрабатываем назначение свободных программистов на проекты в моб-отделе
 
-        myCompany.departments["MobDept"].appointmentMobDevelopers();
+        myCompany.departments["MobDept"].appointmentDevelopers();
 
         // Обрабатываем назначение свободных программистов на проекты в отделе тестирования
 
@@ -45,34 +36,27 @@ function main(n) {
         // Проходимся по веб-проектам с нулевой сложностью, сплайсим и пушим проекты и разработчиков
 
         if (myCompany.departments["WebDept"].getProjectsWithComplexityNull().length) {
-            myCompany.departments["WebDept"].cleanClosedProjects();
             myCompany.departments["WebDept"].moveWebAndMobDevelopers();
+            myCompany.departments["QADept"].receivingWebAndMobProjects(myCompany.departments["WebDept"].getProjectsWithComplexityNull());
+            myCompany.departments["WebDept"].cleanClosedProjects();
+            myCompany.departments["WebDept"].cleanFreeDevelopers();
         }
 
-        else {
-            console.log("Array with web-projects is empty");
-        }
-
-        // Проходимся по  моб-проектам с нулевой сложностью, сплайсим и пушим проекты и разработчиков
+        // Проходимся по моб-проектам с нулевой сложностью, сплайсим и пушим проекты и разработчиков
 
         if (myCompany.departments["MobDept"].getProjectsWithComplexityNull().length) {
-            myCompany.departments["MobDept"].cleanClosedProjects();
             myCompany.departments["MobDept"].moveWebAndMobDevelopers();
-        }
-
-        else {
-            console.log("Array with mob-projects is empty");
+            myCompany.departments["QADept"].receivingWebAndMobProjects(myCompany.departments["MobDept"].getProjectsWithComplexityNull());
+            myCompany.departments["MobDept"].cleanClosedProjects();
+            myCompany.departments["MobDept"].cleanFreeDevelopers();
         }
 
         // Проходимся по проектам (QA) с нулевой сложностью, сплайсим и пушим проекты и разработчиков
 
         if (myCompany.departments["QADept"].getProjectsWithComplexityNull().length) {
-            myCompany.departments["QADept"].cleanClosedProjects();
             myCompany.departments["QADept"].moveQADevelopers();
-        }
-
-        else {
-            console.log("Array with QA is empty");
+            myCompany.departments["QADept"].cleanClosedProjects();
+            myCompany.departments["WebDept"].cleanFreeDevelopers();
         }
 
         // Удаляем разработчика, у которого дни простоя = 3 (самого неопытного)
@@ -87,8 +71,9 @@ function main(n) {
             myCompany.departments[department].addDeveloper();
         }
 
-        n--;
+        n --;
         console.log("***end Day ***");
+        console.log("\n ||\n ||\n");
     }
 }
-main(3);
+main(20);
