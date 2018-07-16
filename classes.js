3dce0df9b613bcd725263b6c1016d6246a169697
Unknown <alyonabill3@mail.ru>
2018-07-16 14:05:43 +0300

    classes in ES5 standart

--- classes.js
+++ classes.js
@@ -1,389 +1,274 @@
-"use strict";
-
-class Company {
-    constructor(departments, director) {
-        this.departments = departments;
-        this.director = director;
-    }
-
-    hiringDevelopers(company){
-        for (let department in company) {
-            company[department].addDeveloper();
-        }
-    }
-
-    dismissingDevelopers(company) {
-        for (let department in company) {
-            company[department].delDeveloper();
-        }
-    }
-
-    decreaseProjComplexity() {
-        for (let dept in this.departments) {
-            this.departments[dept].reduceComplexityProjects();
-        }
-    }
-
-    working(dir, depts, web, mob, QA) {
-        // Генерация проектов на каждый день. Заполнение массивов в объектах отделов
-
-        dir.getProjects(web.projectsInQueue, mob.projectsInQueue);
-
-        // Обрабатываем назначение свободных программистов на проекты в отделах
-
-        dir.setDevelopers(depts);
-
-        //  Уменьшаем сложность у проектов в прогрессе
-
-        this.decreaseProjComplexity();
-
-        // Проходимся по проектам с нулевой сложностью, сплайсим и пушим проекты и разработчиков
-
-        web.projectsProcessing(QA);
-        mob.projectsProcessing(QA);
-        QA.projectsProcessing();
-
-        // Удаляем разработчика, у которого дни простоя = 3 (самого неопытного)
-
-        this.dismissingDevelopers(depts);
-
-        // Нанимаем разработчиков
-
-        this.hiringDevelopers(depts);
-    }
+function Company(departments, director) {
+    this.departments = departments;
+    this.director = director;
 }
 
-let maxNumberProjectsPerDay = 4;
-
-class Director {
-    constructor() {
-        this.projectsCounter = 0;
+Company.prototype.hiringDevelopers = function(company) {
+    for (var department in company) {
+        company[department].addDeveloper();
     }
+};
 
-    createNewProject() {
-        this.projectsCounter++;
-
-        return Math.random() > 0.5 ? new WebProject(this.projectsCounter): new MobProject(this.projectsCounter);
+Company.prototype.dismissingDevelopers = function(company) {
+    for (var department in company) {
+        company[department].delDeveloper();
     }
+};
 
-    getProjects(webDeptQueue, mobDeptQueue) {
-        let projectsCount = Math.floor(Math.random() * maxNumberProjectsPerDay);
-
-        while (projectsCount) {
-            let project = this.createNewProject();
-
-            if (project instanceof WebProject) {
-                webDeptQueue.push(project);
-            }
-            else {
-                mobDeptQueue.push(project);
-            }
-
-            projectsCount--;
-        }
+Company.prototype.decreaseProjComplexity = function() {
+    for (var dept in this.departments) {
+        this.departments[dept].reduceComplexityProjects();
     }
+};
 
-    setDevelopers(depts) {
-        for (let department in depts) {
-            depts[department].appointmentDevelopers();
-        }
-    }
+Company.prototype.working = function(dir, depts, web, mob, QA) {
+    // Генерация проектов на каждый день. Заполнение массивов в объектах отделов
+
+    dir.getProjects(web.projectsInQueue, mob.projectsInQueue);
+
+    // Обрабатываем назначение свободных программистов на проекты в отделах
+
+    dir.setDevelopers(depts);
+
+    //  Уменьшаем сложность у проектов в прогрессе
+
+    this.decreaseProjComplexity();
+
+    // Проходимся по проектам с нулевой сложностью, сплайсим и пушим проекты и разработчиков
+
+    web.projectsProcessing(QA);
+    mob.projectsProcessing(QA);
+    QA.projectsProcessing();
+
+    // Удаляем разработчика, у которого дни простоя = 3 (самого неопытного)
+
+    this.dismissingDevelopers(depts);
+
+    // Нанимаем разработчиков
+
+    this.hiringDevelopers(depts);
+};
+
+var maxNumberProjectsPerDay = 4;
+
+function Director() {
+    this.projectsCounter = 0;
 }
 
-class Project {
-    constructor(id) {
-        this.id = id;
-        this.complexity = this.getRandComplexity();
-    }
+Director.prototype.createNewProject = function() {
+    this.projectsCounter++;
 
-    getRandComplexity() {
-        return Math.floor(Math.random() * 3) + 1;
-    }
-}
+    return Math.random() > 0.5 ? new WebProject(this.projectsCounter): new MobProject(this.projectsCounter);
+};
 
-class WebProject extends Project {}
+Director.prototype.getProjects = function(webDeptQueue, mobDeptQueue) {
+    var projectsCount = Math.floor(Math.random() * maxNumberProjectsPerDay);
 
-class MobProject extends Project {}
+    while (projectsCount) {
+        var project = this.createNewProject();
 
-class Department {
-    constructor() {
-        this.projectsInQueue = [];
-        this.projectsInProgress = [];
-        this.freeDevelopers = [];
-        this.busyDevelopers = [];
-        this.developerToHire = 0;
-        this.developersCounter = 0;
-        this.dismissedDevelopers = 0;
-        this.hiredDevelopers = 0;
-        this.completedProjects = 0;
-    }
-
-    //  Добавляем разработчиков
-
-    addDeveloper() {
-        while (this.developerToHire) {
-            this.developersCounter++;
-
-            this.freeDevelopers.push(new Developer(this.developersCounter));
-
-            this.developerToHire--;
-            this.hiredDevelopers++;
-        }
-    }
-
-    compareNumberDoneProjects(a, b) {
-        return a.numberDoneProjects - b.numberDoneProjects;
-    }
-
-    // Удаляем разработчиков, у которых дни простоя = 3
-
-    delDeveloper() {
-        let developersForDismissArr = this.freeDevelopers.filter(function (developer) {
-            return developer.daysIdled === 3;
-        });
-
-        if (developersForDismissArr.length) {
-            let sortDevelopers = developersForDismissArr.sort(this.compareNumberDoneProjects);
-
-            this.freeDevelopers.splice(this.freeDevelopers.indexOf(this.getDeveloperById(sortDevelopers[0].id)), 1);
-
-            this.dismissedDevelopers++;
-        }
-    }
-
-    // Возвращаем разработчика, у которого указан передаваемый id проекта
-
-    getDeveloperByProject (projectId) {
-        return this.busyDevelopers.find(function (developer) {
-            return developer.currentProject === projectId;
-        });
-    }
-
-    // Возвращаем разработчика, у которого указан передаваемый id разработчика
-
-    getDeveloperById (developerId) {
-        return this.freeDevelopers.find(function (developer) {
-            return developer.id === developerId;
-        });
-    }
-
-    // Возвращаем проекты, у которых сложность = 0
-
-    getWebAndMobClosedProjects () {
-        return this.projectsInProgress.filter(function (project) {
-            return project.complexity === 0;
-        });
-    }
-
-    appointDeveloper () {
-        // Удаляем разработчика из массива свободных
-
-        const dev = this.freeDevelopers.shift();
-
-        // Удаляем проект из массива очереди
-
-        const project = this.projectsInQueue.shift();
-
-        // Обнуляем у разработчика счетчик дней простоя
-
-        dev.daysIdled = 0;
-
-        // Присваиваем разработчику id текущего проекта
-
-        dev.currentProject = project.id;
-
-        // Добавляем разработчика в массив занятых
-
-        this.busyDevelopers.push(dev);
-
-        // Добавляем проект в массив проектов в работе
-
-        this.projectsInProgress.push(project);
-    }
-
-    // Обрабатываем назначение свободных программистов на проекты
-
-    appointmentDevelopers () {
-        while (this.projectsInQueue.length && this.freeDevelopers.length) {
-            this.appointDeveloper();
-        }
-
-        if (this.projectsInQueue.length) {
-            this.developerToHire = this.projectsInQueue.length;
+        if (project instanceof WebProject) {
+            webDeptQueue.push(project);
         }
         else {
-            this.freeDevelopers.forEach(function (item) {
-                item.daysIdled++;
-            });
+            mobDeptQueue.push(project);
         }
+
+        projectsCount--;
     }
+};
 
-    // Уменьшаем сложность проектов
-
-    reduceComplexityProjects() {
-        this.projectsInProgress.forEach(function (project) {
-            project.complexity--;
-        });
+Director.prototype.setDevelopers = function(depts) {
+    for (var department in depts) {
+        depts[department].appointmentDevelopers();
     }
+};
 
-    // Проходим по массиву с нулевыми проектами, обнуляем текущие проекты у разработчиков и добавляем их в freeDevelopers
-
-    moveDevsToFree () {
-        let nullComplexityProjectsArr = this.getWebAndMobClosedProjects();
-
-        nullComplexityProjectsArr.forEach((project) => {
-            let currentDeveloper = this.getDeveloperByProject(project.id);
-            currentDeveloper.currentProject = 0;
-            this.freeDevelopers.push(currentDeveloper);
-        });
-    }
-
-    // Удаляем проекты с нулевой сложностью
-
-    cleanClosedProjects() {
-        for (let index = 0; index < this.projectsInProgress.length; index++) {
-            if (this.projectsInProgress[index].complexity === 0) {
-                this.projectsInProgress.splice(index, 1);
-                index--;
-            }
-        }
-    }
-
-    // Удаляем освободившихся разработчиков из массива занятых
-
-    cleanFreeDevelopers() {
-        for (let i = 0; i < this.busyDevelopers.length; i++) {
-            if (this.busyDevelopers[i].currentProject === 0) {
-                this.busyDevelopers[i].numberDoneProjects++;
-                this.busyDevelopers.splice(i,1);
-                i--;
-            }
-        }
-    }
-
-    projectsProcessing(QADept) {
-        if (this.getWebAndMobClosedProjects().length) {
-            this.moveDevsToFree();
-            QADept.receivingWebAndMobProjects(this.getWebAndMobClosedProjects());
-            this.cleanClosedProjects();
-            this.cleanFreeDevelopers();
-        }
-    }
+function Project(id) {
+    this.id = id;
+    this.complexity = this.getRandComplexity();
 }
 
-class WebDepartment extends Department {}
+Project.prototype.getRandComplexity = function() {
+    return Math.floor(Math.random() * 3) + 1;
+};
 
-class MobDepartment extends Department {
-    appointDeveloper (n, projectId) {
-        while(n) {
-            const dev = (function(){this.freeDevelopers.shift();});
-
-            dev.daysIdled = 0;
-            dev.currentProject = projectId;
-
-            this.busyDevelopers.push(dev);
-            n--;
-        }
-        const project = this.projectsInQueue.shift();
-
-        this.projectsInProgress.push(project);
-    }
-
-    // Обрабатываем назначение свободных моб-программистов на проекты
-
-    appointmentDevelopers() {
-        for (let i = 0; i < this.projectsInQueue.length; i++) {
-            if (this.projectsInQueue[i].complexity === 1 && this.freeDevelopers.length) {
-                this.appointDeveloper(1, this.projectsInQueue[i].id);
-                i--;
-            }
-            else if (this.projectsInQueue[i] === 2 && this.freeDevelopers.length >= 2) {
-                this.appointDeveloper(2, this.projectsInQueue[i].id);
-                i--;
-            }
-            else if (this.projectsInQueue[i] === 3 && this.freeDevelopers.length >= 3) {
-                this.appointDeveloper(3, this.projectsInQueue[i].id);
-                i--;
-            }
-            else if (this.freeDevelopers.length && this.projectsInQueue.length) {
-                while(this.freeDevelopers.length) {
-                    const dev = this.freeDevelopers.shift();
-
-                    dev.daysIdled = 0;
-                    dev.currentProject = this.projectsInQueue[i].id;
-
-                    this.busyDevelopers.push(dev);
-                }
-                const project = this.projectsInQueue.shift();
-
-                this.projectsInProgress.push(project);
-                i--;
-            }
-            else if (this.projectsInQueue.length) {
-                this.developerToHire = this.projectsInQueue.length;
-            }
-            else {
-                this.freeDevelopers.forEach(function (item) {
-                    item.daysIdled ++;}
-                );
-            }
-        }
-    }
+function WebProject () {
+    Project.apply(this, arguments);
 }
 
-class QADepartment extends Department {
-    // Добавление нулевых проектов из веб и моб отделов в отдел тестирования
+WebProject.prototype = Object.create(Project.prototype);
+WebProject.prototype.constructor = WebProject;
 
-    receivingWebAndMobProjects(projectsNullComplexity) {
-        this.projectsInQueue = this.projectsInQueue.concat(projectsNullComplexity);
+function MobProject () {
+    Project.apply(this, arguments);
+}
+
+MobProject.prototype = Object.create(Project.prototype);
+MobProject.prototype.constructor = MobProject;
+
+function Department() {
+    this.projectsInQueue = [];
+    this.projectsInProgress = [];
+    this.freeDevelopers = [];
+    this.busyDevelopers = [];
+    this.developerToHire = 0;
+    this.developersCounter = 0;
+    this.dismissedDevelopers = 0;
+    this.hiredDevelopers = 0;
+    this.completedProjects = 0;
+}
+
+//  Добавляем разработчиков
+
+Department.prototype.addDeveloper = function() {
+    while (this.developerToHire) {
+        this.developersCounter++;
+
+        this.freeDevelopers.push(new Developer(this.developersCounter));
+
+        this.developerToHire--;
+        this.hiredDevelopers++;
+    }
+};
+
+Department.prototype.compareNumberDoneProjects = function(a, b) {
+    return a.numberDoneProjects - b.numberDoneProjects;
+};
+
+// Удаляем разработчиков, у которых дни простоя = 3
+
+Department.prototype.delDeveloper = function() {
+    var developersForDismissArr = this.freeDevelopers.filter(function (developer) {
+        return developer.daysIdled === 3;
+    });
+
+    if (developersForDismissArr.length) {
+        var sortDevelopers = developersForDismissArr.sort(this.compareNumberDoneProjects);
+
+        this.freeDevelopers.splice(this.freeDevelopers.indexOf(this.getDeveloperById(sortDevelopers[0].id)), 1);
+
+        this.dismissedDevelopers++;
+    }
+};
+
+// Возвращаем разработчика, у которого указан передаваемый id проекта
+
+Department.prototype.getDeveloperByProject = function(projectId) {
+    return this.busyDevelopers.find(function (developer) {
+        return developer.currentProject === projectId;
+    });
+};
+
+// Возвращаем разработчика, у которого указан передаваемый id разработчика
+
+Department.prototype.getDeveloperById = function(developerId) {
+    return this.freeDevelopers.find(function (developer) {
+        return developer.id === developerId;
+    });
+};
+
+// Возвращаем проекты, у которых сложность = 0
+
+Department.prototype.getWebAndMobClosedProjects = function() {
+    return this.projectsInProgress.filter(function (project) {
+        return project.complexity === 0;
+    });
+};
+
+Department.prototype.appointDeveloper = function() {
+    // Удаляем разработчика из массива свободных
+
+    var dev = this.freeDevelopers.shift();
+
+    // Удаляем проект из массива очереди
+
+    var project = this.projectsInQueue.shift();
+
+    // Обнуляем у разработчика счетчик дней простоя
+
+    dev.daysIdled = 0;
+
+    // Присваиваем разработчику id текущего проекта
+
+    dev.currentProject = project.id;
+
+    // Добавляем разработчика в массив занятых
+
+    this.busyDevelopers.push(dev);
+
+    // Добавляем проект в массив проектов в работе
+
+    this.projectsInProgress.push(project);
+};
+
+// Обрабатываем назначение свободных программистов на проекты
+
+Department.prototype.appointmentDevelopers = function() {
+    while (this.projectsInQueue.length && this.freeDevelopers.length) {
+        this.appointDeveloper();
+    }
+
+    if (this.projectsInQueue.length) {
         this.developerToHire = this.projectsInQueue.length;
     }
-
-    getClosedProjects () {
-        return this.projectsInProgress.filter(function (project) {
-            return project.complexity === -1;
+    else {
+        this.freeDevelopers.forEach(function (item) {
+            item.daysIdled++;
         });
     }
+};
 
-    // Проходим по массиву с проектами со сложностью -1, обнуляем текущие проекты у разработчиков и добавляем их в freeDevelopers
+// Уменьшаем сложность проектов
 
-    moveDevsToFree () {
-        let projectsWithComplexityNull = this.getClosedProjects();
+Department.prototype.reduceComplexityProjects = function() {
+    this.projectsInProgress.forEach(function (project) {
+        project.complexity--;
+    });
+};
 
-        projectsWithComplexityNull.forEach((project) => {
-            let currentDeveloper = this.getDeveloperByProject(project.id);
+// Проходим по массиву с нулевыми проектами, обнуляем текущие проекты у разработчиков и добавляем их в freeDevelopers
 
-            currentDeveloper.currentProject = 0;
-            this.freeDevelopers.push(currentDeveloper);
-        });
-    }
+Department.prototype.moveDevsToFree = function() {
+    var nullComplexityProjectsArr = this.getWebAndMobClosedProjects();
 
-    cleanClosedQAProjects() {
-        for (let index = 0; index < this.projectsInProgress.length; index++) {
-            if (this.projectsInProgress[index].complexity === -1) {
-                this.projectsInProgress.splice(index, 1);
-                this.completedProjects++;
-                index--;
-            }
+    nullComplexityProjectsArr.forEach((project) => {
+        var currentDeveloper = this.getDeveloperByProject(project.id);
+        currentDeveloper.currentProject = 0;
+        this.freeDevelopers.push(currentDeveloper);
+    });
+};
+
+// Удаляем проекты с нулевой сложностью
+
+Department.prototype.cleanClosedProjects = function() {
+    for (var index = 0; index < this.projectsInProgress.length; index++) {
+        if (this.projectsInProgress[index].complexity === 0) {
+            this.projectsInProgress.splice(index, 1);
+            index--;
         }
     }
+};
 
-    projectsProcessing() {
-        if (this.getClosedProjects().length) {
-            this.moveDevsToFree();
-            this.cleanClosedQAProjects();
-            this.cleanFreeDevelopers();
+// Удаляем освободившихся разработчиков из массива занятых
+
+Department.prototype.cleanFreeDevelopers = function() {
+    for (var i = 0; i < this.busyDevelopers.length; i++) {
+        if (this.busyDevelopers[i].currentProject === 0) {
+            this.busyDevelopers[i].numberDoneProjects++;
+            this.busyDevelopers.splice(i,1);
+            i--;
         }
     }
-}
+};
 
-class Developer {
-    constructor(id) {
-        this.id = id;
-        this.currentProject = 0;
-        this.numberDoneProjects = 0;
-        this.daysIdled = 0;
+Department.prototype.projectsProcessing = function(QADept) {
+    if (this.getWebAndMobClosedProjects().length) {
+        this.moveDevsToFree();
+        QADept.receivingWebAndMobProjects(this.getWebAndMobClosedProjects());
+        this.cleanClosedProjects();
+        this.cleanFreeDevelopers();
     }
-}
+};
 
-module.exports = {Company, Director, Department, WebDepartment, MobDepartment, QADepartment, WebProject, MobProject};
+module.exports = {Company, Director, Department};
